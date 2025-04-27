dbe418eced8beb075a810d38d674f366eb009468
mauriciodelizxavier <mauricioxavier409@outlook.com>
2025-04-27 19:25:48 -0300

    Arquivos Iniciais de Estudo

---
+++ src/com/mauricioxavier/maratonajava/introducao/OlaMundo.java
@@ -0,0 +1,14 @@
+package com.mauricioxavier.maratonajava.introducao;
+/**
+ * Isso é um comentário <b> Javadoc</b>
+ */
+public class OlaMundo {
+    // Comentário de uma linha
+    /*
+        Comentario de
+        múltiplas linhas
+     */
+    public static void main(String[] args) {
+        System.out.println("Olá Mundo, esta é a minha primeira aula da Maratona!");
+    }
+}
