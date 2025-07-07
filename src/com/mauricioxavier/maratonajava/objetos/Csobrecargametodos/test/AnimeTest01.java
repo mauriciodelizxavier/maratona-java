1428a0d8ae9379821507c87c19910304198d4020
mauriciodelizxavier <mauricioxavier409@outlook.com>
2025-07-07 10:51:20 -0300

    feature: Sobrecarga de métodos

---
+++ src/com/mauricioxavier/maratonajava/objetos/Csobrecargametodos/test/AnimeTest01.java
@@ -0,0 +1,13 @@
+package com.mauricioxavier.maratonajava.objetos.Csobrecargametodos.test;
+
+import com.mauricioxavier.maratonajava.objetos.Csobrecargametodos.dominio.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+        Anime anime = new Anime();
+
+        anime.init("Naruto", "TV", 15, "Ação");
+        anime.imprimir();
+
+    }
+}
