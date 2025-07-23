9eee887bc42d1d64445a84e181c0b8b02ae185b7
mauriciodelizxavier <mauricioxavier409@outlook.com>
2025-07-22 21:49:01 -0300

    feature: Modificador Final

---
+++ src/com/mauricioxavier/maratonajava/objetos/Jmodificadorfinal/test/CarroTest01.java
@@ -0,0 +1,10 @@
+package com.mauricioxavier.maratonajava.objetos.Jmodificadorfinal.test;
+
+import com.mauricioxavier.maratonajava.objetos.Jmodificadorfinal.dominio.Carro;
+
+public class CarroTest01 {
+    public static void main(String[] args) {
+        Carro carro = new Carro();
+        System.out.println(Carro.VELOCIDADE_LIMITE);
+    }
+}
